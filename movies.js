7861ae0ece9f6d3dc3eef11cd112e99c594e6d43
Lili Myers <lilianna.myers@kellogg.northwestern.com>
2021-02-25 07:52:22 -0800

    WIP

--- movies.js
+++ movies.js
@@ -47,81 +47,43 @@ letauthUIConfig = {
     ],
     signInSuccessURL: 'movies.html'
 }
-
-
+// FirebaseUI Auth
+ui.start('.sign-in-or-sign-out', authUIConfig)
 }
 
-}
+// loop movie list 
+for (let i=0; i<movies.length; i++) {
+    let movie = movies[i]
 
+    // check db for movie watched by signed in user
+    let docRef = await db.collection('watched').doc(`${movie.id}-${users.uid}`).get()
+    let watchedMovie = docRef.data()
+    let opacityClass = ''
 
-
-
-
-
-
-
-
-
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-window.addEventListener('DOMContentLoaded', async function(event) {
-    let db = firebase.firestore()
-    let apiKey = 'your TMDB API key'
-    let response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}&language=en-US`)
-    let json = await response.json()
-    let movies = json.results
-    console.log(movies)
-    
-    for (let i=0; i<movies.length; i++) {
-      let movie = movies[i]
-      let docRef = await db.collection('watched').doc(`${movie.id}`).get()
-      let watchedMovie = docRef.data()
-      let opacityClass = ''
-      if (watchedMovie) {
+    //opacity if movie watched
+    if (watchedMovie) {
         opacityClass = 'opacity-20'
-      }
-  
-      document.querySelector('.movies').insertAdjacentHTML('beforeend', `
+    }
+
+    document.querySelector('.movies').insertAdjacentHTML('beforeend', `
         <div class="w-1/5 p-4 movie-${movie.id} ${opacityClass}">
           <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="w-full">
           <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
         </div>
       `)
-  
+
+      // when user clicks watched button, assign opacity and write to db
       document.querySelector(`.movie-${movie.id}`).addEventListener('click', async function(event) {
         event.preventDefault()
         let movieElement = document.querySelector(`.movie-${movie.id}`)
         movieElement.classList.add('opacity-20')
-        await db.collection('watched').doc(`${movie.id}`).set({})
-      }) 
+
+        // assign document ID using movie ID & user ID
+        await db.collection('watched').doc(`${movie.id}-${users.uid}`).set({})
+        })
     }
-  })
+})
+
   
   // Goal:   Refactor the movies application from last week, so that it supports
   //         user login and each user can have their own watchlist.
